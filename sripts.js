136eeeddfca3fc695cf42213528d739baa57e6f2
karire amandine <karireamandine@gmail.com>
2018-12-14 15:44:42 +0200

    add the function of getting the result

---
+++ sripts.js
@@ -0,0 +1,8 @@
+function getResult(){
+    var side1=parsefloat(document.getElementById('side1').value);
+    var side2=parsefloat(document.getElementById('side2').value);
+    var side3=parsefloat(document.getElementById('side3').value);
+if(side1===side2 && side2===side3){
+    alert ("This is an equilateral triangle");
+}
+}
