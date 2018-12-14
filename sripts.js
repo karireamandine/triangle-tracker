cd7d2e34de0495951c2a50a461c4ea5692cac618
karire amandine <karireamandine@gmail.com>
2018-12-14 16:12:13 +0200

    add the alert as not definite triangle

--- sripts.js
+++ sripts.js
@@ -8,5 +8,8 @@ if(side1===side2 && side2===side3){
 else if (side1===side2 || side1===side3 || side2===side3){
     alert ("This is an isoscle triangle");
 }
+else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
+    alert("Not a definite triangle");
+}
 
 }
