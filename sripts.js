ba43668c0a0c4df32f731647ea120846c2ce2ca4
karire amandine <karireamandine@gmail.com>
2018-12-17 12:05:40 +0200

    add a link in javascript

--- sripts.js
+++ sripts.js
@@ -1,17 +1,21 @@
 function getResult(){
-    var side1=parsefloat(document.getElementById('side1').value);
-    var side2=parsefloat(document.getElementById('side2').value);
-    var side3=parsefloat(document.getElementById('side3').value);
+    var side1=parseFloat(document.getElementById('side1').value);
+    var side2=parseFloat(document.getElementById('side2').value);
+    var side3=parseFloat(document.getElementById('side3').value);
 if(side1===side2 && side2===side3){
     alert ("This is an equilateral triangle");
+    window.location.href="equilateral.html"
 }
 else if (side1===side2 || side1===side3 || side2===side3){
-    alert ("This is an isoscle triangle");
+    alert ("This is an isoscele triangle");
+    window.location.href="isoscele.html"
 }
 else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
     alert("Not a definite triangle");
+    window.location.href="undefined-triangle.html"
 }
 else{
     alert("This is a scalene triangle");
+    window.location.href="scalene.html"
 }
 }
